e826c84250ef5d2719da8e4be9ed2b8c2739ee51
Brandon <brandon139@protonmail.com>
2024-02-25 17:16:39 +1100

    first

---
+++ cmd/golang-web-srs/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	//"fmt"
+
+	"github.com/labstack/echo/v4"
+	"github.com/Brandon689/golang-web-srs/internal/routes"
+	"github.com/Brandon689/golang-web-srs/internal/db"
+)
+
+func main() {
+
+
+	err := db.InitDB()
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	//  sqlDB, _ := dbt.DB.DB()
+	//  defer sqlDB.Close()
+
+
+
+
+	db.CreateProduct()
+	db.CreateProduct()
+	db.CreateProduct()
+	db.CreateProduct()
+
+
+
+
+
+	
+	// m := db.NewDb()
+	// fmt.Println(m.Goal())
+
+	//c := routes.NewRoute(m)
+	c := routes.NewRoute()
+	e := echo.New()
+
+	// Register routes from another file
+	c.RegisterRoutes(e)
+	//fmt.Println(c.LLL())
+
+	// Start the server on port 8080
+	e.Start(":8080")
+
+}
